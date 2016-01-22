9dd64a3b8caaddd864aaa0f9a424a18e869aee21
Alisson Bruno <alissonbruno.sa@gmail.com>
2016-01-21 23:24:08 -0300

    Utilizando ajax para criação e remoção de post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
+
+  private
+  def after_sign_in_path_for(_resource_or_scope)
+    home_index_path
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,11 @@
 class PostsController < ApplicationController
   def create
-    @post = current_user.posts.build(post_params)
-    if @post.save
-      redirect_to home_index_path
-    else
-      render 'home/index'
-    end
+    @post = current_user.posts.create(post_params)
   end
 
   def destroy
     @post = current_user.posts.find(params[:id])
     @post.destroy
-    redirect_to home_index_path
   end
 
 
